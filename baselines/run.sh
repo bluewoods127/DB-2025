75619cfa1e062855dbeb6b60f8212c62433aa63e
Fatemeh Doudi <fatemehdoudi@gmail.com>
2025-05-22 10:40:10 -0500

    Update run.sh

--- baselines/run.sh
+++ baselines/run.sh
@@ -8,4 +8,13 @@ python3 -u run_RS.py \
 --t2i_path 'path_to_checkpoint/pytorch_lora_weights.bin' \
 --vila_path 'path_to_checkpoint/pytorch_lora_weights.bin'
 
+source activate #same_env_as_DB
+python3 -u run_CoDe.py \
+--cache_dir ".cache/" \
+--ir_weight 0.2 \
+--prompt_file "dataset/test_prompts.json" \
+--n_samples 20 \
+--block_size 5 \
+
+
 
